d89327b4f8fdd35fd17ac49f3f60648de540b451
VladKouz <kuznetmail@gmail.com>
2026-07-01 22:57:32 +0300

    Linter ruff - well done

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -1,6 +1,8 @@
 import prompt
 
+
 def get_user_name():
+
     user_name = prompt.string('May I have your name? ')
     print(f'Hello, {user_name}!')
     return user_name
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,10 @@
 from brain_games import cli
 
+
 def main():
+
     print("Welcome to the Brain Games!")
-    user_name = cli.get_user_name()
+    cli.get_user_name()
 
 
 if __name__ == "__main__":
